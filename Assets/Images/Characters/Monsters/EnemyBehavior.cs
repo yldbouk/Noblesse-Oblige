9dce2f09a1ad8dc94d14c1286f2d1231d5d4e22a
Matthew Donoho <116532065+DonohoM@users.noreply.github.com>
2023-05-04 07:59:11 -0500

    made prefabs better :)

--- Assets/Images/Characters/Monsters/EnemyBehavior.cs
+++ Assets/Images/Characters/Monsters/EnemyBehavior.cs
@@ -7,9 +7,21 @@ public class EnemyBehavior : MonoBehaviour
     SpriteRenderer sprite;
     GameObject target;
 
-    public float speed;
-    public float distanceThreshold;
-    public float attackCooldown;
+    [SerializeField] float speed;
+    [SerializeField] float distanceThreshold;
+    [SerializeField] float attackCooldown;
+    [SerializeField] bool invertAxis;
+
+    private bool attacking = false;
+
+    [SerializeField] Transform attackPoint1;
+    [SerializeField] Transform attackPoint2;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] int attackDamage = 40;
+    [SerializeField] float attackRange = 0.5f;
+    [SerializeField] float attackRate = 2f;
+
+    private bool direction;
 
     bool readyToAttack = true;
 
@@ -29,9 +41,9 @@ public class EnemyBehavior : MonoBehaviour
         if (Vector2.Distance(transform.position, target.transform.position) > distanceThreshold)
         {
             animator.SetBool("isRunning", true);
-            int direction = (transform.position.x < target.transform.position.x ? 1 : -1);
-            sprite.flipX = transform.position.x > target.transform.position.x;
-            transform.position = new Vector2(transform.position.x + direction * speed /100, transform.position.y);
+            direction = (transform.position.x < target.transform.position.x);
+            sprite.flipX = (transform.position.x > target.transform.position.x) ^ invertAxis;
+            transform.position = new Vector2(transform.position.x + (direction ? 1 : -1) * speed /100, transform.position.y);
             return;
         }
         readyToAttack = false;
@@ -40,14 +52,40 @@ public class EnemyBehavior : MonoBehaviour
     }
 
     private IEnumerator Attack() 
-    {  
+    {
+        // Play an attack animation
         animator.SetBool("isRunning", false);
         animator.SetBool("isNearPlayer", true);
+
+        // Detect enemies in range of attack
+
+        Collider2D[] hitEnemies1 = Physics2D.OverlapCircleAll(attackPoint1.position, attackRange, enemyLayer);
+        Collider2D[] hitEnemies2 = Physics2D.OverlapCircleAll(attackPoint2.position, attackRange, enemyLayer);
+
+        // Damage them
+        foreach (Collider2D enemy in hitEnemies1)
+        {
+            enemy.GetComponent<enemy>().TakeDamage(attackDamage);
+        }
+        foreach (Collider2D enemy in hitEnemies2)
+        {
+            enemy.GetComponent<enemy>().TakeDamage(attackDamage);
+        }
+
         yield return new WaitForSeconds(.25f);
         animator.SetBool("isNearPlayer", false);
         yield return new WaitForSeconds(attackCooldown);
         readyToAttack = true;
     }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint1 == null && attackPoint2 == null)
+            return;
 
-    
+        if (!direction)
+            Gizmos.DrawWireSphere(attackPoint2.position, attackRange);
+        else
+            Gizmos.DrawWireSphere(attackPoint1.position, attackRange);
+    }
+
 }
--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -16,6 +16,7 @@ public class enemy : MonoBehaviour
 
     [SerializeField] AudioClip DieClip;
     [SerializeField] AudioClip HitClip;
+   
 
     // Start is called before the first frame update
     void Start()
